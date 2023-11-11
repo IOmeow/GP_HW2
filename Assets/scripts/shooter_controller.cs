b7289712d6a1a591350bacae4e679093a2e2eaea
panroger <panroger66@gmail.com>
2023-11-11 20:46:42 +0800

    fix shooter bug

--- Assets/scripts/shooter_controller.cs
+++ Assets/scripts/shooter_controller.cs
@@ -8,7 +8,10 @@ public class shooter_controller : monster
 
     private bool shooting = false;
     private float cd = 2.3f;
+    private float ani_start_to_shoot = 0.9f;
     private float prev_shooting_time;
+    private bool waiting_for_ani = false;
+    private bool do_shoot = false;
 
     private int bullet_speed = 10;
     public GameObject hand;
@@ -27,40 +30,48 @@ public class shooter_controller : monster
             // 面向玩家
             var rot = look_at_player();
 
+            // 已經開始射擊
+            if(shooting){
+                if(waiting_for_ani && Time.time - prev_shooting_time >= ani_start_to_shoot){
+                    shoot();
+                    waiting_for_ani = false;
+                    do_shoot = true;
+                }
+                else if(Time.time - prev_shooting_time >= cd){
+                    do_shoot = false;
+                    waiting_for_ani = true;
+                    prev_shooting_time = Time.time;
+                }
+            }
+
             // 還沒開始射擊, 且距離上一發時間夠長
-            if(!shooting && Time.time - prev_shooting_time >= cd){
+            else if(!shooting && Time.time - prev_shooting_time >= cd){
                 // 開始射擊
-                //InvokeRepeating("shoot", rot / rotate_ang * Time.fixedDeltaTime, cd);
-                //InvokeRepeating("shoot", calculate_delay(rot), cd);
-                StartCoroutine(shoot());
+                animator.SetBool("attack",true);
                 shooting = true;
+                waiting_for_ani = true;
+
+                // update last time we shoot
+                prev_shooting_time = Time.time;
             }
         }
         
         // 正在射擊, 但玩家距離太遠
         else if(shooting){
-            //CancelInvoke("shoot");
-            StopCoroutine(shoot());
             shooting = false;
             animator.SetBool("attack",false);
+
+            if(!do_shoot) prev_shooting_time -= cd;
         }
     }
 
     // 射擊
-    private IEnumerator shoot(){
-        while(true){
-            animator.SetBool("attack",true);
-            yield return new WaitForSeconds(0.90f);
+    private void shoot(){
+        // create bullet
+        var bullet_rb = Instantiate(bullet_pref, hand.transform.position, Quaternion.identity)
+            .GetComponent<Rigidbody>();
 
-            // create bullet
-            var bullet_rb = Instantiate(bullet_pref, hand.transform.position, Quaternion.identity)
-                .GetComponent<Rigidbody>();
-            // give the bullet initial velocity
-            bullet_rb.velocity = transform.forward * bullet_speed;
-
-            // update last time we shoot
-            prev_shooting_time = Time.time;
-            yield return new WaitForSeconds(cd-0.90f);
-        }
+        // give the bullet initial velocity
+        bullet_rb.velocity = transform.forward * bullet_speed;
     }
 }
--- Assets/scripts/snowhitcontrol.cs
+++ Assets/scripts/snowhitcontrol.cs
@@ -5,20 +5,8 @@ using UnityEngine;
 
 public class snowhitcontrol : MonoBehaviour
 {
-    // Start is called before the first frame 
-    private float Timer =7f;
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Timer-=Time.fixedDeltaTime;
-        if(Timer<=0)
-        {
-            Destroy(gameObject);
-        }   
+        Destroy(gameObject, 1);
     }
 }
